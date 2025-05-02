9dea662eaf40ff38938a8a024dcfef13464d1768
waqar321 <waqarmughal707@gmail.com>
2025-05-02 16:25:08 +0500

    aaadawd

--- app/Http/Livewire/Admin/Ledger/Index.php
+++ app/Http/Livewire/Admin/Ledger/Index.php
@@ -5,6 +5,8 @@ namespace App\Http\Livewire\Admin\Ledger;
 use Livewire\Component;
 use App\Models\Admin\ItemCategory;
 use App\Models\Admin\Item;
+use App\Models\User;
+use App\Models\Role;
 use Livewire\WithPagination;
 use Livewire\WithFileUploads;
 use App\Traits\livewireComponentTraits\LedgerComponent;
@@ -21,7 +23,8 @@ class Index extends Component
                             'selectedColumns' => 'export',
                             // 'CategoryOperation' => 'DeleteCategory'
                             'deleteCategoryOperation' => 'DeleteCategory',
-                            'getItemAmount' => 'fetchItemAmount'
+                            'getItemAmount' => 'fetchItemAmount',
+                            'UpdateFields' => 'HandleUpdateFields'
                         ];
 
     public function mount()
@@ -53,18 +56,184 @@ class Index extends Component
         $this->dispatchBrowserEvent('item_price', ['item_price' => $this->itemAmount]);
     }   
 
-    public function saveCategory()
+    public function saveLedger()
     {
-        $this->ItemCategory->save();
-        $this->ItemCategory = new ItemCategory();
+        // if($this->selected_role_id != null)
+        // {
+        //     $this->Ledger->role_id = $this->selected_role_id;
+        // }
+        if($this->selected_user_id != null)
+        {
+            $this->Ledger->user_id = $this->selected_user_id;
+        }
+        if($this->selected_item_id != null)
+        {
+            $this->Ledger->item_id = $this->selected_item_id;
+        }
+        if($this->selected_payment_id != null)
+        {
+            $this->Ledger->payment_id = $this->selected_payment_id;
+        }
+
+        // dd($this->Ledger);
+
+
+        $this->Ledger->save();
+        $this->Ledger = new Ledger();
         $this->Collapse = 'collapse';
 
         if($this->Update)
         {
-            $this->dispatchBrowserEvent('ItemCategoryUpdated', ['message' => 'Category updated succesfullyy!!']);
+            $this->dispatchBrowserEvent('LedgerUpdated', ['message' => 'Category updated succesfullyy!!']);
         }
 
-        // dd($this->ItemCategory);
+        // dd($this->Ledger);
 
+    }
+    public function HandleUpdateFields($data_id, $value)
+    { 
+        //dd($data_id, $value);
+        // dd($data_id == 'payment_type', $data_id);    
+        if($data_id == 'user_role')
+        {
+            $this->selected_role_id = $value;
+
+            // if(empty($this->selected_role_id))
+            // {
+                $this->ResetData();
+                $this->RenderData();
+            // }
+
+            $this->payment_type_show = '';
+            $this->Collapse = 'uncollapse';
+            // $this->dispatchBrowserEvent('LoadedUsers', ['users' => $users]);
+        }
+        else if($data_id == 'payment_type')
+        {
+            $this->selected_payment_id = $value;
+
+            // show selected role users 
+            if (in_array($this->selected_role_id, [12, 13, 14, 15]))  // 8 => "Manager" , // 12 => "Employee" // 13 => "Cashier" // 14 => "Customer" // 15 => "Vendor"
+            {
+                $users = User::whereHas('roles', function ($query) {
+                    $query->where('id', $this->selected_role_id);
+                })->pluck('name', 'id');
+            }
+            
+            if($this->selected_payment_id == 'cash')
+            {
+                $this->cash_amount_show = '';
+                $this->total_amount_show = '';   
+            }
+            else if($this->selected_payment_id == 'product_sold')
+            {
+                $items = Item::pluck('name', 'id');
+
+                $users = User::whereHas('roles', function ($query) {
+                    $query->where('id', $this->selected_role_id);
+                })->pluck('name', 'id');
+            }
+            else if($this->selected_payment_id == 'product_bought')
+            {
+                if($this->selected_role_id == 15) // from vendor role we can bought only
+                {
+                    $items = Item::where('category_id', 12)->pluck('name', 'id');
+                }
+                else
+                {
+                    $this->dispatchBrowserEvent('item_error', ['message' => 'We Can buy only from vendors']);
+                    return false;
+                }
+            }
+
+            $this->show_users = '';
+            $this->Collapse = 'uncollapse';
+            $this->dispatchBrowserEvent('LoadedUsers', ['users' => $users]);    
+
+            if($this->selected_payment_id == 'product_sold' || $this->selected_payment_id == 'product_bought')
+            {
+                $this->show_items = '';
+                $this->show_items = '';
+                $this->unit_price_show = '';
+                $this->unit_price_read_only = 'readonly';
+                $this->unit_qty_show = '';
+                $this->total_amount_show = '';
+                $this->dispatchBrowserEvent('LoadedItems', ['items' => $items]);
+            }
+
+            // $this->payment_type_show = '';
+            // dd($this->selected_payment_id);
+            // dd('seleceted payment_type ', $value);
+            
+        }
+        else if($data_id == 'user_id')
+        {
+            $this->selected_user_id = $value;
+            // dd('seleceted user_id ', $value);
+
+        }
+        else if($data_id == 'item_id')
+        {
+            $this->selected_item_id = $value;
+            $selected_item = Item::find($this->selected_item_id);
+            $this->Ledger->unit_price = $selected_item->price;
+            $this->Ledger->unit_qty = 1;
+            $this->Ledger->total_amount = $this->Ledger->unit_price * $this->Ledger->unit_qty;
+        }
+    }
+    public function ResetDataForCash()
+    {
+        //add d-none reset user
+        //add d-none reset item
+        //add d-none reset unit price
+        //add d-none reset unit qty
+        //add d-none reset unit total amount
+
+        $this->payment_type_show = 'd-none';
+        $this->show_users = 'd-none';
+        $this->show_items = 'd-none';
+        $this->cash_amount_show = 'd-none';
+        $this->unit_price_show = 'd-none';
+        $this->unit_qty_show = 'd-none';
+        $this->total_amount_show = 'd-none';
+        $this->payment_detail_show = 'd-none';
+
+        $this->selected_item_id = null;
+        $this->selected_role_id = null;
+        $this->selected_user_id = null;
+        $this->selected_payment_id = null;
+
+        $this->unit_price_read_only = '';
+        
+        $this->Ledger->cash_amount = null;
+        $this->Ledger->unit_price = null;
+        $this->Ledger->unit_qty = null;
+        $this->Ledger->total_amount = null;
+        $this->Ledger->payment_detail = null;
+    }
+    public function ResetData()
+    {
+        $this->payment_type_show = 'd-none';
+        $this->show_users = 'd-none';
+        $this->show_items = 'd-none';
+        $this->cash_amount_show = 'd-none';
+        $this->unit_price_show = 'd-none';
+        $this->unit_qty_show = 'd-none';
+        $this->total_amount_show = 'd-none';
+        $this->payment_detail_show = 'd-none';
+
+        $this->selected_item_id = null;
+        $this->selected_role_id = null;
+        $this->selected_user_id = null;
+        $this->selected_payment_id = null;
+
+        $this->unit_price_read_only = '';
+        
+        $this->Ledger->cash_amount = null;
+        $this->Ledger->unit_price = null;
+        $this->Ledger->unit_qty = null;
+        $this->Ledger->total_amount = null;
+        $this->Ledger->payment_detail = null;
+        
     }
 }
--- resources/views/Admin/Ledger/add.blade.php
+++ resources/views/Admin/Ledger/add.blade.php
@@ -21,48 +21,52 @@
 
                 <div class="col-mb-12 col-lg-12">
 
-                    <form wire:submit.prevent="saveCategory">
+                    <form wire:submit.prevent="saveLedger">
 
                         @csrf
                         <!-- =========================== title ========================== -->
-                        <div class="col-md-6 col-sm-6 col-xs-12" >
+                        <div class="col-md-6 col-sm-6 col-xs-12 {{ $user_role_show }}" >
                             <div class="form-group" wire:ignore>
                                 <label for="user_role">Select User Role</label>
-                                <select class="form-control" id="user_role" name="user_role" required>
-                                    <option value="">-- Choose Role --</option>
-                                    <option value="customer">Customer</option>
-                                    <option value="cashier">Cashier</option>
-                                    <option value="boss">Boss</option>
-                                    <option value="vendor">Vendor</option>
+                                <!-- <select class="form-control Select2DropDown" data-id="user_role" id="user_role" name="user_role"> -->
+                                <select class="form-control Select2DropDown" data-id="user_role" id="user_role" name="user_role">
+                                    <option value="">-- Select Role --</option>
+                                        @foreach($roles as $role_id => $role_title)
+                                            <option value="{{ $role_id }}">{{ $role_title }}</option>
+                                        @endforeach 
                                 </select>
                                 <span class="error-container danger w-100"></span>
                             </div>
                         </div>
-                        <div class="col-md-6 col-sm-6 col-xs-12" wire:ignore>
-                            <div class="form-group" id="cash_amount_section" >
-                                <label for="cash_amount">Cash Amount</label>
-                                <input type="number" class="form-control" id="cash_amount" name="amount" value="0.0" step="0.01">
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-sm-6 col-xs-12" >
-                            <div class="form-group" wire:ignore>
 
+                        <div class="col-md-6 col-sm-6 col-xs-12 {{ $payment_type_show }}" >
+                            <div class="form-group" wire:ignore>
                                 <label for="payment_type">Payment Type</label>
-                                <select class="form-control" id="payment_type" name="payment_type" required>
+                                <select class="form-control Select2DropDown" data-id="payment_type" id="payment_type" name="payment_type">
                                     <option value="">-- Select Payment Type --</option>
                                     <option value="cash">Cash</option>
-                                    <option value="product_sold">Product Sold</option>
-                                    <option value="product_bought">Product Bought</option>
+                                    <option value="product_sold">Product Sale</option>
+                                    <option value="product_bought">Product Buy</option>
                                 </select>
                                 <span class="error-container danger w-100"></span>
                             </div>
                         </div>
-
-
-                        <div class="col-md-6 col-lg-6" wire:ignore>
-                            <div class="form-group" id="vendor_item_section" >
+                      
+                        <div class="col-md-6 col-sm-6 col-xs-12 {{ $show_users }}" >
+                            <div class="form-group" wire:ignore>
+                                <label>user <span class="danger">*</span></label>
+                                <!-- <option value="">-- Select user --</option> -->
+                                <select class="form-control Select2DropDown" data-id="user_id" id="user_id" name="user_id">
+                                        @foreach($users as $user_id => $name)
+                                            <option value="{{ $user_id }}">{{ $name }}</option>
+                                        @endforeach 
+                                </select>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-sm-6 col-xs-12 {{ $show_items }}" >
+                            <div class="form-group" wire:ignore>
                                 <label>Item <span class="danger">*</span></label>
-                                <select name="item_id" id="item_id" class="form-control">
+                                <select class="form-control Select2DropDown" data-id="item_id" id="item_id" name="item_id" >
                                     <option value="">-- Select Item --</option>
                                         @foreach($items as $item_id => $item_name)
                                             <option value="{{ $item_id }}">{{ $item_name }}</option>
@@ -70,34 +74,38 @@
                                 </select>
                             </div>
                         </div>
-
-                        <div class="col-md-6 col-sm-6 col-xs-12" wire:ignore>
+                        <div class="col-md-6 col-sm-6 col-xs-12 {{ $cash_amount_show }}" >
+                            <div class="form-group" id="cash_amount_section" >
+                                <label for="cash_amount">Cash Amount</label>
+                                <input type="number" class="form-control" wire:model='Ledger.cash_amount' id="cash_amount" name="amount" min="0" step="1" value="0">
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-sm-6 col-xs-12 {{ $unit_price_show }}" >
                             <div class="form-group" id="unit_price_section" >
                                 <label for="unit_price">Unit Price</label>
-                                <input type="number" class="form-control" id="unit_price" name="unit_price" step="0.01" value="0.0">
+                                <input type="number" class="form-control" wire:model='Ledger.unit_price' {{ $unit_price_read_only }} id="unit_price" name="unit_price" min="0" step="1" value="0">
                                 <span class="error-container danger w-100"></span>
                             </div>
                         </div>
-                        <div class="col-md-6 col-sm-6 col-xs-12" wire:ignore>
+                        <div class="col-md-6 col-sm-6 col-xs-12 {{ $unit_qty_show }}" >
                             <div class="form-group" id="unit_qty_section" >
                                 <label for="unit_qty">Unit Qty</label>
-                                <input type="number" class="form-control" id="unit_qty" name="unit_qty" min="0" max="100" step="1" value="0">
-
+                                <input type="number" class="form-control" wire:model='Ledger.unit_qty' id="unit_qty" name="unit_qty" min="1" step="1">
                                 <!-- <input type="number" class="form-control" id="unit_qty" name="unit_qty" step="0.01" value="0.0"> -->
                                 <span class="error-container danger w-100"></span>
                             </div>
                         </div>
-                        <div class="col-md-6 col-sm-6 col-xs-12" wire:ignore>
+                        <div class="col-md-6 col-sm-6 col-xs-12 {{ $total_amount_show }}" >
                             <div class="form-group" id="total_amount_section" >
                                 <label for="total_amount" class="mt-2">Total Amount</label>
-                                <input type="text" class="form-control" id="total_amount" name="amount" value="0.0" readonly>
+                                <input type="text" class="form-control" wire:model='Ledger.total_amount' id="total_amount" name="amount" min="0" step="1" readonly>
                             </div>
                         </div>
 
-                        <div class="col-md-6 col-sm-6 col-xs-12" >
-                            <div class="form-group" wire:ignore>
+                        <div class="col-md-6 col-sm-6 col-xs-12 {{ $payment_detail_show }}" >
+                            <div class="form-group">
                                 <label for="details">Payment Details</label>
-                                <textarea class="form-control" name="details" id="details" rows="2" required></textarea>
+                                <textarea class="form-control" name="details" wire:model='Ledger.payment_detail' id="details" min="0" step="1"></textarea>
                                 <span class="error-container danger w-100"></span>
                             </div>
                         </div>
